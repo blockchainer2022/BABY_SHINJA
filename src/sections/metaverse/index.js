73697c6b58d84bf283d783e0322a7325500ff924
coders003 <94458357+coders003@users.noreply.github.com>
2022-01-04 20:26:14 +0530

    update

--- src/sections/metaverse/index.js
+++ src/sections/metaverse/index.js
@@ -7,8 +7,8 @@ const Metaverse = () => {
       <div className="container">
         <div>
           <Title title="The INU Metaverse" backtext="METAVERSE" />
-          <p className="text-center mt-6 text-sm md:mx-20">
-            CAPTAIN INU STRIVES TO CREATE A METAVERSE WORLD FOR ITS INU ARMY,
+          <p className="text-center mt-6 text-sm md:mx-20 uppercase">
+            Baby Shinja STRIVES TO CREATE A METAVERSE WORLD FOR ITS INU ARMY,
             BRINGING THE COMMUNITY TOGETHER IN ONE ENGAGING AND IMMERSIVE
             ECOSYSTEM
           </p>
@@ -23,21 +23,21 @@ const Metaverse = () => {
           >
             <p className="text-sm capitalize font-normal mt-6 text-dark-300">
               Bringing the community together in one engaging and immersive
-              metaverse ecosystem, aka Captain Inuverse. A play to earn Inu
-              gaming metaverse is currently being formed by the Captain Inu
+              metaverse ecosystem, aka Baby Shinjaverse. A play to earn Inu
+              gaming metaverse is currently being formed by the Baby Shinja
               team, which has partnered up with a well-known gaming company that
               has nine years of gaming development experience.
             </p>
             <p className="text-sm capitalize font-normal mt-6 text-dark-300">
               One of our partners has even received a variety of international
-              gaming awards! The team’s next gaming venture is Captain Inuverse,
-              making Captain’s Inu story come to life. In Captain Inuverse,
-              known Shiba Inu projects will be presented, uniting the whole Inu
+              gaming awards! The team’s next gaming venture is Baby Shinjaverse,
+              making Baby Shinja story come to life. In Baby Shinjaverse, known
+              Shiba Inu projects will be presented, uniting the whole Inu
               community.
             </p>
             <p className="text-sm capitalize font-normal mt-6 text-dark-300">
               People can role play with their favorite Inu character during the
-              missions. Captain Inuverse will be an open-world fantasy game that
+              missions. Baby Shinjaverse will be an open-world fantasy game that
               gets built on the Ethereum blockchain.
             </p>
             {/* <button className=" bg-primary-500 text-dark-300 py-2 px-6 mt-8 rounded-md font-bold">
@@ -47,11 +47,11 @@ const Metaverse = () => {
         </div>
         <p className="text-sm capitalize  font-normal mt-10 text-dark-300">
           The game will be a merging environment through PVP battle gaming and
-          an open-world exploration game. Captain Inuverse will be implementing
+          an open-world exploration game. Baby Shinjaverse will be implementing
           on-chain interactions (ETH Network) and exclusive NFT in-game items to
           be collected by the players. The idea is to create virtual 3d
-          galleries, displaying Captain Inu NFT’s. These NFTs will be developed
-          by various NFT artists, and the talent of the Captain Inu community.
+          galleries, displaying Baby Shinja NFT’s. These NFTs will be developed
+          by various NFT artists, and the talent of the Baby Shinja community.
         </p>
       </div>
     </div>
